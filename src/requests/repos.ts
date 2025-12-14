fc62f7f249f94d2ea8d1d0c121b0fef0ace2a33a
RecursivePineapple <recursive_pineapple@proton.me>
2025-12-14 16:33:42 -0500

    Fix repo url

--- src/requests/repos.ts
+++ src/requests/repos.ts
@@ -82,12 +82,12 @@ export async function clone_repo(repo_id: RepoId, checkout: boolean = true): Pro
     }
 
     if (is_gh_action) {
-        await exec(`gh repo clone git:${gh_token}@github.com/${owner}/${repo}.git ${repo_path} --  ${checkout ? "" : "--no-checkout"}`, {
+        await exec(`git clone git:${gh_token}@github.com:${owner}/${repo}.git ${repo_path} --  ${checkout ? "" : "--no-checkout"}`, {
             cwd: clone_scratchpad,
             env: { GH_TOKEN: gh_token }
         });
     } else {
-        await exec(`gh repo clone git@github.com/${owner}/${repo}.git ${repo_path} --  ${checkout ? "" : "--no-checkout"}`, {
+        await exec(`git clone git@github.com:${owner}/${repo}.git ${repo_path} --  ${checkout ? "" : "--no-checkout"}`, {
             cwd: clone_scratchpad,
             env: { GH_TOKEN: gh_token }
         });
